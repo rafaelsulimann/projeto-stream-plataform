9870420882f5773a5c22c4ab0f5ea21efcd34142
Rafael Sulimann <sulimannrafael@gmail.com>
2023-08-07 21:16:15 -0300

    Criação do projeto

---
+++ authuser/src/main/java/com/bottle/authuser/AuthuserApplication.java
@@ -0,0 +1,13 @@
+package com.bottle.authuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthuserApplication.class, args);
+	}
+
+}
