5bb614692c396068caf3a7fd594ba7f33d48e1b0
Rafael Sulimann <sulimannrafael@gmail.com>
2023-08-15 21:35:39 -0300

    Chore: Criação dos microservições de service registry, config server e api gateway

---
+++ config-server/src/main/java/com/bottle/configserver/configs/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.bottle.configserver.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Value("${bottle.configServer.username}")
+    private String username;
+
+    @Value("${bottle.configServer.password}")
+    private String password;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.httpBasic()
+            .and()
+            .authorizeRequests().anyRequest().authenticated()
+            .and()
+            .csrf().disable()
+            .formLogin();
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService (){
+        UserDetails user = User.withUsername(username).password(passwordEncoder().encode(password)).roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user);
+    }
+    
+}
